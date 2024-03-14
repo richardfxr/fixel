c13991457fd4cd4bc9c3634ebc357bedb48f0bf2
Richard Fu <richardfxr.personal@gmail.com>
2024-03-14 18:36:29 -0400

    🟠SVLT: create CanvasControls component & mode variable

---
+++ src/lib/canvasControls.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+    /* === IMPORTS ============================ */
+    import type { Mode } from "../routes/canvas/+page.svelte";
+
+    /* === PROPS ============================== */
+    export let mode: Mode; // bind
+</script>
+
+
+
+<form>
+    <div role="group" aria-label="Mode:" id="mode">
+        <div>
+            <input
+                type="radio"
+                id="draw"
+                class="visuallyHidden"
+                name="mode"
+                value="draw"
+                bind:group={mode} />
+            <label for="draw">Draw</label>
+        </div>
+        <div>
+            <input
+                type="radio"
+                id="move"
+                class="visuallyHidden"
+                name="mode"
+                value="move"
+                bind:group={mode} />
+            <label for="move">Move</label>
+        </div>
+    </div>
+</form>
+
+
+
+<style lang="scss">
+    $_clr-600: #6e6e6e;
+    $_clr-bg: #ffffff;
+
+    form {
+        display: flex;
+        flex-flow: column nowrap;
+        align-items: center;
+        position: fixed;
+        top: 10px;
+        right: 0;
+        left: 0;
+        z-index: 2;
+        
+        padding: 0 10px;
+        pointer-events: none;
+
+        & > div {
+            display: flex;
+            flex-flow: row wrap;
+            width: 100%;
+            max-width: 300px;
+            background-color: $_clr-bg;
+
+            pointer-events: auto;
+        }
+
+        #mode {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+
+            border: solid 1px $_clr-600;
+
+            input:checked + label {
+                color: white;
+                background-color: $_clr-600;
+            }
+
+            label {
+                display: block;
+                width: 100%;
+                padding: 6px 15px;
+                text-align: center;
+
+                color: $_clr-600;
+                cursor: pointer;
+            }
+        }
+    }
+</style>
--- src/routes/canvas/+page.svelte
+++ src/routes/canvas/+page.svelte
@@ -1,11 +1,20 @@
+<script lang="ts" context="module">
+    /* === TYPES ============================== */
+    export type Mode = "draw" | "move";
+</script>
+
 <script lang="ts">
     /* === IMPORTS ============================ */
     import Canvas from "$lib/canvas.svelte";
+    import CanvasControls from "$lib/canvasControls.svelte";
 
     /* === CONSTANTS ========================== */
-    const width = 6;
-    const height = 6;
+    const width = 9;
+    const height = 9;
+
+    /* === VARIABLES ========================== */
     let image = generateGrid(width, height);
+    let mode: Mode = "draw";
 
     /* === FUNCTIONS ========================== */
     function generateGrid(width: number, height: number): Uint8ClampedArray {
@@ -23,16 +32,21 @@
 
 
 <main>
+    <CanvasControls
+        bind:mode={mode} />
+    
     <Canvas
         {width}
         {height}
-        bind:image={image} />
+        bind:image={image}
+        {mode} />
 </main>
 
 
 
 <style lang="scss">
     main {
+        color: black;
         background-color: white;
     }
 </style>
