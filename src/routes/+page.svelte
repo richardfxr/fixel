9834957719539b6aeb2569bf8be844a37eccc206
Richard Fu <richardfxr.personal@gmail.com>
2024-03-14 13:44:19 -0400

    🟠SVLT: create basic placeholder page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <h1>Fixel</h1>
+    <p>Currently in development.</p>
+</main>
+
+
+
+
+<style lang="scss">
+    main {
+        display: flex;
+        flex-flow: column nowrap;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+
+        padding: 20px 15px;
+    }
+
+    h1, p {
+        text-align: center;
+    }
+</style>
